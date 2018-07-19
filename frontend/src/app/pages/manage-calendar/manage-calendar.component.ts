c597cbc7e9e22994263ece91a4ccdc33efa214b1
mihaig <mihai.gorgos@gmail.com>
2018-07-19 22:32:14 +0300

    adaugat "manage-calendar"

---
+++ frontend/src/app/pages/manage-calendar/manage-calendar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'az-manage-calendar',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './manage-calendar.component.html',
+  styleUrls: ['./manage-calendar.component.scss']
+})
+export class ManageCalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
