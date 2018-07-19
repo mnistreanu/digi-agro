0506385c61f35302777643366f2a18472ecfe8c6
maxim.nistreanu <maxim.nistreanu@arobs.com>
2018-07-19 10:02:26 +0300

    simplified custom repository files (without interfaces);

--- src/main/java/com/arobs/repository/custom/MapEventCustomRepository.java
+++ src/main/java/com/arobs/repository/custom/MapEventCustomRepository.java
@@ -1,8 +1,35 @@
 package com.arobs.repository.custom;
 
-
 import com.arobs.model.UpdateFieldModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
 
-public interface MapEventCustomRepository {
-    void update(UpdateFieldModel model);
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+
+@Repository
+public class MapEventCustomRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Modifying
+    public void update(UpdateFieldModel model) {
+
+        String queryStr = "UPDATE MapEvent e SET e." + model.getField() + " = ";
+
+
+        if (model.getValue() == null) {
+            queryStr += " null ";
+        }
+        else {
+            queryStr += " '" + model.getValue() + "' ";
+        }
+
+        queryStr += " WHERE e.id = :id";
+
+        em.createQuery(queryStr).setParameter("id", model.getId())
+                .executeUpdate();
+    }
 }
--- src/main/java/com/arobs/repository/custom/TelemetryCustomRepository.java
+++ src/main/java/com/arobs/repository/custom/TelemetryCustomRepository.java
@@ -1,7 +1,26 @@
 package com.arobs.repository.custom;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.math.BigDecimal;
 
-public interface TelemetryCustomRepository {
-    void updateCoordinate(Long id, String field, BigDecimal value);
+@Repository
+public class TelemetryCustomRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Modifying
+    public void updateCoordinate(Long id, String field, BigDecimal value) {
+
+        String queryStr = "UPDATE Telemetry t SET t." + field + " = :value WHERE t.id = :id";
+
+        em.createQuery(queryStr)
+                .setParameter("value", value)
+                .setParameter("id", id)
+                .executeUpdate();
+    }
 }
