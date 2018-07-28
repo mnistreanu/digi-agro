9fd3db7d91eafc51b5470653a7e666528e186ad6
maxim.nistreanu <maxim.nistreanu@arobs.com>
2018-07-28 15:55:43 +0300

    frontend: possibility to select tenant;

---
+++ frontend/src/app/services/auth/auth-response.model.ts
@@ -0,0 +1,4 @@
+export class AuthRequestModel {
+    username: String;
+    password: String;
+}
--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -10,6 +10,8 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Authorities} from "../common/authorities";
 import {UserAccountModel} from "../pages/manage-users/user/user-account.model";
 import {LangService} from "./lang.service";
+import {ListItem} from "../interfaces/list-item.interface";
+import {StorageService} from "./storage.service";
 
 
 @Injectable({
@@ -19,7 +21,7 @@ export class AuthService {
 
     private api: string = Constants.API_URL + "/auth";
 
-    userChangedChanged: EventEmitter<void> = new EventEmitter();
+    userChanged: EventEmitter<void> = new EventEmitter();
 
     private httpOptions = {
         headers: new HttpHeaders({
@@ -29,44 +31,42 @@ export class AuthService {
 
     constructor(private http: HttpClient,
                 private router: Router,
+                private storageService: StorageService,
                 private langService: LangService,
                 private errorService: ErrorService) {
     }
 
     logout() {
-        localStorage.removeItem(Constants.USER_DATA);
-        localStorage.removeItem(Authorities.AUTHORITY_OBJECT);
-        this.langService.clear();
-        this.userChangedChanged.emit();
+        this.storageService.clear();
+        this.langService.restoreLanguage();
+        this.userChanged.emit();
     }
 
-    login(model: UserAuth): Observable<boolean> {
+    login(model: UserAuth): Observable<ListItem[]> {
         return this.http.post<boolean>(this.api + '/login', model)
             .map((response) => {
                 let token = response['token'];
                 let authorities = response['authorities'];
                 let logoUrl = response['logoUrl'];
                 let language = response['language'];
-                if (token) {
-                    let userData = {username: model.username, token: token, logoUrl: logoUrl};
-                    localStorage.setItem(Constants.USER_DATA, JSON.stringify(userData));
-                    this.langService.setLanguage(language);
-                    this.setupAuthorities(authorities);
-                    return true;
-                }
-                this.userChangedChanged.emit();
-                return false;
+                let userData = {username: model.username, token: token, logoUrl: logoUrl};
+                this.storageService.setItem(Constants.USER_DATA, JSON.stringify(userData));
+                this.langService.setLanguage(language);
+                this.setupAuthorities(authorities);
+                this.userChanged.emit();
+
+                return response['tenants'];
             });
     }
 
     setupAuthorities(authorities: string[]) {
-        localStorage.setItem(Authorities.AUTHORITY_OBJECT, '{}');
+        this.storageService.setItem(Authorities.AUTHORITY_OBJECT, '{}');
         let authorityObject = {};
         authorities.forEach(authority => {
             authorityObject[authority] = true;
         });
-        localStorage.setItem(Authorities.AUTHORITY_OBJECT, JSON.stringify(authorityObject));
-        this.userChangedChanged.emit();
+        this.storageService.setItem(Authorities.AUTHORITY_OBJECT, JSON.stringify(authorityObject));
+        this.userChanged.emit();
     }
 
     checkAuth() {
@@ -76,11 +76,11 @@ export class AuthService {
     }
 
     isAuthenticated(): boolean {
-        return !!localStorage.getItem(Constants.USER_DATA);
+        return !!this.storageService.getItem(Constants.USER_DATA);
     }
 
     getUsername(): string {
-        let userData = JSON.parse(localStorage.getItem(Constants.USER_DATA));
+        let userData = JSON.parse(this.storageService.getItem(Constants.USER_DATA));
         if (userData) {
             return userData.username;
         }
@@ -88,7 +88,7 @@ export class AuthService {
     }
 
     getUserData() {
-        return JSON.parse(localStorage.getItem(Constants.USER_DATA));
+        return JSON.parse(this.storageService.getItem(Constants.USER_DATA));
     }
 
     fetchCurrentUser(): Observable<UserAccountModel> {
@@ -96,7 +96,7 @@ export class AuthService {
     }
 
     getToken(): String {
-        let userData = JSON.parse(localStorage.getItem(Constants.USER_DATA));
+        let userData = JSON.parse(this.storageService.getItem(Constants.USER_DATA));
         return userData && userData.token ? userData.token : "";
     }
 
@@ -107,7 +107,7 @@ export class AuthService {
     }
 
     hasAuthority(authorityName) {
-        let authorityObject = localStorage.getItem(Authorities.AUTHORITY_OBJECT);
+        let authorityObject = this.storageService.getItem(Authorities.AUTHORITY_OBJECT);
         return this.isAuthenticated() && JSON.parse(authorityObject)[authorityName];
     }
 
@@ -120,11 +120,11 @@ export class AuthService {
     }
 
     updateUser(model: UserAccountModel) {
-        let userData = JSON.parse(localStorage.getItem(Constants.USER_DATA));
+        let userData = JSON.parse(this.storageService.getItem(Constants.USER_DATA));
         userData.logoUrl = model.logoUrl;
-        localStorage.setItem(Constants.USER_DATA, JSON.stringify(userData));
+        this.storageService.setItem(Constants.USER_DATA, JSON.stringify(userData));
         this.langService.setLanguage(model.language);
-        this.userChangedChanged.emit();
+        this.userChanged.emit();
     }
 
 }
