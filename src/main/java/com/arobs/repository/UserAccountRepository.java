69dd6506ca1cdd4704072964a96d143dc9b4b715
isirena <Iurie.Sirena@gmail.com>
2018-08-26 09:01:20 +0300

    Intimplator UserAccountRepository s-a transferat in "weather"

--- src/main/java/com/arobs/repository/UserAccountRepository.java
+++ src/main/java/com/arobs/repository/UserAccountRepository.java
@@ -1,7 +1,6 @@
-package com.arobs.weather.snapshot;
+package com.arobs.repository;
 
 import com.arobs.entity.UserAccount;
-import com.arobs.enums.AuthorityName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
