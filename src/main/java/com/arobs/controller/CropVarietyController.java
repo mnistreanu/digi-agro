13a766dd66e20acb0b4beb95f3013335ee226140
Alex <alex.cigureanu@atsr.ie>
2018-08-29 20:21:42 +0100

    CropVarieties backend

---
+++ src/main/java/com/arobs/controller/CropVarietyController.java
@@ -0,0 +1,82 @@
+package com.arobs.controller;
+
+
+import com.arobs.entity.Crop;
+import com.arobs.entity.CropVariety;
+import com.arobs.model.CropModel;
+import com.arobs.model.CropVarietyModel;
+import com.arobs.service.CropVarietyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/crop_varieties")
+public class CropVarietyController {
+
+    @Autowired
+    private CropVarietyService cropVarietyService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        cropVarietyService.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<CropVarietyModel> save(@RequestBody CropVarietyModel model) {
+        return ResponseEntity.ok(new CropVarietyModel(cropVarietyService.save(model)));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<CropVarietyModel> findById(@PathVariable("id") final Long id) {
+
+        CropVariety cropVariety = cropVarietyService.findOne(id);
+
+        if (cropVariety != null) {
+            return ResponseEntity.ok(new CropVarietyModel(cropVariety));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<CropVarietyModel> update(@PathVariable("id") final Long id, @RequestBody final CropVarietyModel resource) {
+
+        CropVariety cropVariety = cropVarietyService.findOne(id);
+
+        if (cropVariety == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(new CropVarietyModel(cropVarietyService.save(resource)));
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<String> getCrops(@RequestParam(value = "page_no", required = false) final Integer page,
+                                           @RequestParam(value = "rows_per_page", required = false) final Integer rowsPerPage,
+                                           @RequestParam(value = "sort", required = false) final List<String> sorts,
+                                           @RequestParam(value = "filter", required = false) final List<String> filters) {
+
+        if (page != null && rowsPerPage != null && sorts != null && filters != null) {
+            return ResponseEntity.ok(cropVarietyService.findAll(page, rowsPerPage, filters, sorts).toString());
+        }
+
+        if (page != null && rowsPerPage != null && filters != null) {
+            return ResponseEntity.ok(cropVarietyService.findAll(page, rowsPerPage, filters, new ArrayList<>()).toString());
+        }
+
+        if (page != null && rowsPerPage != null) {
+            return ResponseEntity.ok(cropVarietyService.findAll(page, rowsPerPage, new ArrayList<>(), new ArrayList<>()).toString());
+        }
+
+        return ResponseEntity.ok(cropVarietyService.findAll().toString());
+    }
+}
