dd791bf17b534766665620f02f0e47579fb9d587
mihaig <mihai.gorgos@gmail.com>
2018-09-25 21:45:04 +0300

    Categorii de cheltuieli

--- frontend/src/app/pages/enterprise/enterprise.module.ts
+++ frontend/src/app/pages/enterprise/enterprise.module.ts
@@ -12,6 +12,7 @@ import {ImageRendererModule} from '../../modules/aggrid/image-renderer/image-ren
 import {PinnedRowRendererModule} from '../../modules/aggrid/pinned-row-renderer/pinned-row-renderer.module';
 import {PinnedRowRendererComponent} from '../../modules/aggrid/pinned-row-renderer/pinned-row-renderer.component';
 import {EditRendererComponent} from '../../modules/aggrid/edit-renderer/edit-renderer.component';
+import {EditRendererModule} from '../../modules/aggrid/edit-renderer/edit-renderer.module';
 import {ExpenseCategoryTreeComponent} from './manage-expense-categories/expense-category-tree/expense-category-tree.component';
 import {ExpenseCategoryComponent} from './manage-expense-categories/expense-category/expense-category.component';
 
@@ -31,6 +32,7 @@ export const routes = [
         FormErrorBlockModule,
         ImageRendererModule,
         PinnedRowRendererModule,
+        EditRendererModule,
         AgGridModule.withComponents([EditRendererComponent, ImageRendererComponent, PinnedRowRendererComponent]),
         RouterModule.forChild(routes)
     ],
