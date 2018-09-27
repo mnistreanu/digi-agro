b3619f185fa643da1f5e6629a6122b9488ebdce4
isirena <Iurie.Sirena@gmail.com>
2018-09-28 01:56:34 +0300

    Build pentru prod

--- src/main/java/com/arobs/Application.java
+++ src/main/java/com/arobs/Application.java
@@ -4,26 +4,33 @@ import org.jdto.DTOBinder;
 import org.jdto.spring.SpringDTOBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 
 	@Bean
 	public DTOBinder dtoBinder() {
 		return new SpringDTOBinder();
 	}
-	
+
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-	   return builder.build();
+		return builder.build();
 	}
 }
