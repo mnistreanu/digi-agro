f8440c8da5556fa1b5c3cb91eea0706a2c7f7f01
mihaig <mihai.gorgos@gmail.com>
2018-10-29 22:17:49 +0200

    CROP_VARIETY form fixes

--- frontend/src/app/pages/manage-crops/crop-variety/crop-variety.model.ts
+++ frontend/src/app/pages/manage-crops/crop-variety/crop-variety.model.ts
@@ -1,6 +1,8 @@
+import {CropModel} from '../crop/crop.model';
 export class CropVarietyModel {
         public id: number;
         public cropCategoryId: number;
+        public cropModel: CropModel;
         public cropId: number;
         public nameRo: String;
         public nameRu: String;
