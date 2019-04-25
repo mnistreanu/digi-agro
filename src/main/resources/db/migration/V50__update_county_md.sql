75f4094401f431447eb0f36cebaa4df714dabbe1
mihaig <mihai.gorgos@gmail.com>
2019-04-25 13:06:23 +0300

    satele si orasele din Moldova

---
+++ src/main/resources/db/migration/V50__update_county_md.sql
@@ -0,0 +1,7 @@
+DELETE FROM county WHERE country_id='MD' and id='CG';
+DELETE FROM county WHERE country_id='MD' and id='CO';
+DELETE FROM county WHERE country_id='MD' and id='RB';
+DELETE FROM county WHERE country_id='MD' and id='GR';
+DELETE FROM county WHERE country_id='MD' and id='SL';
+
+UPDATE county SET name_ro='Transnistria', name_ru='Приднестровье' WHERE country_id='MD' and id='TS';
